a49346bcf65351232f76f27bf745374383d5ca72
LUIZ HENRIQUE PEREIRA CORREA <luiz_h_correa@estudante.sc.senai.br>
2023-08-25 19:52:58 -0300

    Atualizado blibioteca lombok

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -6,15 +6,10 @@ import br.com.senai.usuariosmktplace.core.service.UsuarioService;
 public class InitApp {
 
 	public static void main(String[] args) {
-		
-		UsuarioService service = new UsuarioService() ;
-		
-		service.criarPor("luiz henrique pereira", "luiz1234");
-		
+				
 		Usuario usuario = new Usuario("luiz.henrique", "luiz123", "luiz henrique");
 		System.out.println(usuario.getLogin());
-		
-		System.out.println("aqui");
+		System.out.println(usuario);
 		
 	}
 
